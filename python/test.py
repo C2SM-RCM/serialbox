d6f97acf24662f0a7daf64a5988b748647a15101
Pascal Spörri <pascal.spoerri@env.ethz.ch>
2015-12-16 11:24:59 +0100

    Adding a python test module

---
+++ python/test.py
@@ -0,0 +1,12 @@
+import unittest
+
+class Test(unittest.TestCase):
+
+	def test_load_serializer(self):
+		from serialbox import Serializer
+
+	def test_load_visualizer(self):
+		from serialbox import Visualizer
+
+if __name__ == '__main__':
+	unittest.main()
